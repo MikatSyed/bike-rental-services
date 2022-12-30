a9c574dcae81c23fbf18a9b109deff8377cafd71
MikatSyed <syednew5000@gmail.com>
2022-12-30 19:56:04 +0600

    bike rental-service

--- backend/app.js
+++ backend/app.js
@@ -14,15 +14,14 @@ const path = require("path");
 // }))
 
 // Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
-}
+
 
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload());
+app.use(fileUpload());   
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 //Route Import
